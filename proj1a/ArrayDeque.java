0565c558a4a184ffdd2129e0c104bda28f473994
23u18u24 <75160282+23u18u24@users.noreply.github.com>
2021-03-31 20:46:41 +0800

    proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,25 +4,26 @@ public class ArrayDeque<T> {
     private int nextFirst = 4;
     private int nextLast = 5;
     private T[] arr;
+    private int mass = 8;
 
     public ArrayDeque() {
         arr = (T[]) new Object[8];
     }
 
     public void addFirst(T item) {
-
         if (size >= 8 && size <= 16) {
-            T[] newarr = (T[]) new Object[size + 1];
+            mass = size + 1;
+            T[] newarr = (T[]) new Object[mass];
             System.arraycopy(arr, nextFirst + 1, newarr, nextFirst + 1, nextLast);
             arr = newarr;
         } else if (size > 16) {
-            T[] newarr = (T[]) new Object[size * 4];
+            mass = size * 4;
+            T[] newarr = (T[]) new Object[mass];
             System.arraycopy(arr, nextFirst + 1, newarr, nextFirst + 1, nextLast);
             arr = newarr;
         }
         if (nextFirst < 0) {
-            addLast(item);
-            return;
+            nextFirst += mass;
         } else {
             arr[nextFirst] = item;
         }
@@ -31,7 +32,6 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T item) {
-        int mass = 8;
         if (size >= 8 && size <= 16) {
             mass = size + 1;
             T[] newarr = (T[]) new Object[mass];
@@ -44,8 +44,7 @@ public class ArrayDeque<T> {
             arr = newarr;
         }
         if (nextLast >= mass) {
-            addFirst(item);
-            return;
+            nextLast -= mass;
         }
         arr[nextLast] = item;
         nextLast += 1;
@@ -67,6 +66,9 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
         T temp = arr[nextFirst + 1];
         arr[nextFirst + 1] = null;
         nextFirst += 1;
@@ -75,6 +77,9 @@ public class ArrayDeque<T> {
     }
 
     public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
         T temp = arr[nextLast - 1];
         arr[nextLast - 1] = null;
         nextLast -= 1;
@@ -83,6 +88,6 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index) {
-        return arr[nextFirst + index];
+        return arr[nextFirst + index + 1];
     }
 }
