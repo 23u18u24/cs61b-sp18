a956a14834a5fe78c00dfe3959477cc2cf9e4377
23u18u24 <75160282+23u18u24@users.noreply.github.com>
2021-05-22 17:50:32 +0800

    hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +13,19 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        List<Integer> buckets = new ArrayList<>();
+        for (Oomage oomage : oomages) {
+            int bucketNum = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            if (!buckets.contains(bucketNum)) {
+                buckets.add(bucketNum);
+            }
+        }
+        int N = oomages.size();
+        for (Integer bucket : buckets) {
+            if (!(bucket < N / 2.5 || bucket > N / 50)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -10,26 +10,42 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
         // TODO: Write this method.
-        return false;
+        if (o == null) {
+            return false;
+        }
+        if (this == o) {
+            return true;
+        }
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        return (red == ((SimpleOomage)o).red) && (green == ((SimpleOomage)o).green) && (blue == ((SimpleOomage)o).blue);
     }
 
     /* Uncomment this method after you've written
        equals and failed the testHashCodeAndEqualsConsistency
-       test.
+       test.*/
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
             // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            int hash = 17;
+            hash += red / 5;
+            hash *= 257;
+            hash += green / 5;
+            hash *= 257;
+            hash += blue / 5;
+            hash *= 257;
+            return hash;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
